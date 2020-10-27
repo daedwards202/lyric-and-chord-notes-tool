b17f365262c88d05103c03b800b1ed1f31a0b95e
DishonoredSavage <d.edwards202@btinternet.com>
2020-10-27 18:38:35 +0000

    - Initial page layout and styling; - Pressing 'Enter' with lyrics typed in the TextField adds them to an array which is mapped to <li> elements.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { TextField } from '@material-ui/core';
+import styled from "styled-components";
+import React, { useState, useReducer } from "react";
+
+export const secondaryColour = '#474853';
+
+const ColorBorderTextField = styled(TextField)`
+  & label.Mui-focused {
+    color: ${secondaryColour};
+  }
+  & .MuiOutlinedInput-root {
+    &.Mui-focused fieldset {
+      border-color: ${secondaryColour};
+    }
+  }
+`;
 
 function App() {
+
+  const [lyrics, dispatchLyrics] = useReducer((lyrics, { type, value }) => {
+    switch (type) {
+      case "add":
+        return [...lyrics, value];
+      case "remove":
+        return lyrics.fiter((_, index) => index !== value);
+      default:
+        return lyrics;
+    }
+  }, []);
+  const [textValue, setTextValue] = useState('');
+
+  const enterNewLine = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      dispatchLyrics({type: "add", value: textValue})
+      setTextValue('')
+    }
+  }
+
+  const updateTextValue = (e) => {
+    setTextValue(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <h1>Lyrics & Chords</h1>
+        <ol id="lyrics">
+          {lyrics.map((lyric, index) =>  
+            <li key={index}>{index+1}: {lyric}</li>
+          )}
+        </ol>
+        <form noValidate autoComplete="off">
+          <ColorBorderTextField 
+            id="outlined-basic" 
+            label="" 
+            value={textValue}
+            multiline 
+            variant="outlined" 
+            fullWidth="true"
+            onChange={updateTextValue}
+            onKeyPress={enterNewLine}
+          />
+        </form>
+      </body>
     </div>
   );
 }
